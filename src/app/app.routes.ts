50922ec058164c075d8dac9d0b523331d97638e8
Amir <amirsiahvashi25@gmail.com>
2024-09-01 10:17:26 +0330

    v1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './+paige/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home' , component:HomeComponent}
+];
